8e98d224f5568c87a2cabac4c89cacd03a736be8
CharanTeja-6825 <rcharanteja2006@gmail.com>
2025-12-30 16:12:32 +0530

    project reset and refactors

--- DRIVEAWAY-FRONTEND/src/App.jsx
+++ DRIVEAWAY-FRONTEND/src/App.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import CustomerNavbar from './navbars/CustomerNavbar'
-import MainNavbar from './navbars/MainNavbar'
 
 function App() {
   return (
-    <div>
-      {/* App Component */}
-      <BrowserRouter >
-        <MainNavbar />
-      </BrowserRouter>
-    </div>
+    <div>App</div>
   )
 }
 
