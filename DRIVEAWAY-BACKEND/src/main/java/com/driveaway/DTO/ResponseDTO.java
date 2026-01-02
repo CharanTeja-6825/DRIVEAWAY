1ca3fa068f07b56e4fe19cf50619abbbe94d2a40
CharanTeja-6825 <rcharanteja2006@gmail.com>
2026-01-02 19:02:23 +0530

    return login object using ResponseDTO

---
+++ DRIVEAWAY-BACKEND/src/main/java/com/driveaway/DTO/ResponseDTO.java
@@ -0,0 +1,39 @@
+package com.driveaway.DTO;
+
+public class ResponseDTO {
+	private String email;
+	private String role;
+	private String token;
+	
+	public ResponseDTO(String email, String role, String token){
+		this.email = email;
+		this.role = role;
+		this.token = token;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
