11f24739dd46b45674ea37b84ca835068dbfdac0
RATHIKINDI CHARAN TEJA <rcharanteja2006@gmail.com>
2026-02-08 23:46:21 +0530

    Update axios baseURL to use import.meta.env

--- DRIVEAWAY-FRONTEND/src/api/axios.js
+++ DRIVEAWAY-FRONTEND/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import logout from '../shared/hooks/useLogout'
 
 const api = axios.create({
-  baseURL: process.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 
