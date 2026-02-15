5d1fcc21e6fd93b51cb4d205db2eefc9a69910e6
Rathikindi Charan Teja <rcharanteja2006@gmail.com>
2026-02-15 16:08:18 +0530

    JwtFilter DEBUG

--- DRIVEAWAY-BACKEND/src/main/java/com/driveaway/config/JwtFilter.java
+++ DRIVEAWAY-BACKEND/src/main/java/com/driveaway/config/JwtFilter.java
@@ -36,6 +36,7 @@ public class JwtFilter extends OncePerRequestFilter{
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 
+		System.out.println(request.getServletPath());
 		Cookie[] cookies = request.getCookies();
 
 		String authHeader = request.getHeader("Authorization");
