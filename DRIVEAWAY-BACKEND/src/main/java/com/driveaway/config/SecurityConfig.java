0ee3a6bb0bce359b97f10330e255ede44af19199
Rathikindi Charan Teja <rcharanteja2006@gmail.com>
2026-02-15 16:03:11 +0530

    CORS Configurations

--- DRIVEAWAY-BACKEND/src/main/java/com/driveaway/config/SecurityConfig.java
+++ DRIVEAWAY-BACKEND/src/main/java/com/driveaway/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList(clientAPI,  "https://script.google.com", "https://script.googleusercontent.com", "https://docs.google.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
